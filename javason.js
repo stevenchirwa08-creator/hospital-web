b11077c3d3aac59553098ea16fcf838cc05a2026
steven <stevenchirwa08@gmail.com>
2026-09-13 09:33:05 +0200

    follow 14

---
+++ javason.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const appointmentForm =
+        document.getElementById("appointmentForm");
+
+    if (appointmentForm) {
+
+        appointmentForm.addEventListener("submit", function (event) {
+
+            event.preventDefault();
+
+            const patientName =
+                document.getElementById("patientName").value;
+
+            const doctor =
+                document.getElementById("doctor").value;
+
+            const date =
+                document.getElementById("date").value;
+
+            const time =
+                document.getElementById("time").value;
+
+
+            alert(
+                "Appointment booked successfully!\n\n" +
+
+                "Patient: " + patientName + "\n" +
+
+                "Doctor: " + doctor + "\n" +
+
+                "Date: " + date + "\n" +
+
+                "Time: " + time
+            );
+
+            appointmentForm.reset();
+
+        });
+
+    }
+
+});
